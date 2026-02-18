1b575ce4961de6e77ad79a8bd44cb65a48f61d45
petermsamba <pmsamba@byupathway.edu>
2026-02-18 15:12:15 +0200

    adding demo video

--- project/scripts/booings.js
+++ project/scripts/booings.js
@@ -1,8 +1,6 @@
 export function initBooking() {
     const form = document.querySelector("#booking-form");
     const nameDisplay = document.querySelector("#welcome-back");
-
-    // Local Storage: Greeting
     const savedName = localStorage.getItem("userName");
     if (savedName && nameDisplay) {
         nameDisplay.textContent = `Welcome back, ${savedName}! Ready to book?`;
@@ -12,7 +10,6 @@ export function initBooking() {
         form.addEventListener("submit", (e) => {
             const firstName = document.querySelector("#fname").value;
             localStorage.setItem("userName", firstName);
-            // Form proceeds to thanks.html
         });
     }
 }
